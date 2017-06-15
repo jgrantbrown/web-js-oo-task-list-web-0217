6576a7e9170d969931481d5d0936199e5636ace3
JGBrown <j.grant.brown@gmail.com>
2017-06-15 15:39:09 -0400

    Done.

--- js/controllers/lists.controller.js
+++ js/controllers/lists.controller.js
@@ -1,3 +1,25 @@
 'use strict';
 // Lists Controller
+function ListsController(){
+  this.$addListForm = $('#add_list');
+  this.$listTitleInput = $('#list_title');
+  this.$selectListMenu =$('#select_list');
+  this.$addTaskForm = $('#add_task');
+  this.$wrapper= $('#wrapper');
+}
 
+ListsController.prototype.init = function() {
+    this.$addTaskForm.hide()
+    this.$addListForm.on('submit', () => this.$addTaskForm.show())
+
+    this.$addListForm.on('submit', (e) => {
+      e.preventDefault()
+      let x = $('#add_list input:first').val()
+      let newList = new List(x)
+      newList.build()
+      $('.destroy-list').on('click', (e)=>{
+        e.preventDefault()
+        e.target.parentElement.parentElement.remove()
+      })
+    })
+  }
--- js/controllers/tasks.controller.js
+++ js/controllers/tasks.controller.js
@@ -1,3 +1,21 @@
 'use strict';
 // Tasks Controller
+function TasksController(){
+  this.$addTaskForm = $('#add_task');
+  this.$taskDescriptionInput = $('#task_description');
+  this.$selectListMenu =$('#select_list');
+  this.$taskPriorityInput =$('#task_priority');
+  this.$wrapper = $('#wrapper');
+}
 
+TasksController.prototype.init = function(){
+  this.$addTaskForm.on('submit', (e) => {
+    e.preventDefault()
+    let list = $('#select_list :selected').text();
+    let desc = this.$taskDescriptionInput.val()
+    let priority = this.$taskPriorityInput.val()
+    debugger
+    let newTask = new Task(desc, priority, list)
+    newTask.build()
+  })
+}
